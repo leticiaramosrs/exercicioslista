692850406072dd482732c0aba6c812704f4065c6
Kamila Richardt <richardtkamila@gmail.com>
2026-03-17 17:06:07 -0300

    Add files via upload
    atividade2

---
+++ atv2.js
@@ -0,0 +1,9 @@
+function vendas(){
+let qtdVendida = 5
+let valorv = 40
+let valorc = 25
+let lucro = valorv - valorc
+let lucroTotal = qtdVendida * lucro
+return lucroTotal
+};
+console.log(vendas())
